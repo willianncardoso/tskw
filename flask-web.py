dc182ae934e69b5ce92ef989c464123411846f5c
williancardosodasilva <williancardoso@ibm.com>
2019-10-11 17:28:58 -0300

    first commit

---
+++ flask-web.py
@@ -0,0 +1,12 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'<h1>Hello, {escape(name)}!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
